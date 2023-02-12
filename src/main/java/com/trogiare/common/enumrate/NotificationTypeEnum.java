b2501699aaf5cf0b9407861421a3854bc086c3ce
Tùng Phạm <77684440+tung-creat@users.noreply.github.com>
2023-02-12 17:57:22 +0700

    change java version

---
+++ src/main/java/com/trogiare/common/enumrate/NotificationTypeEnum.java
@@ -0,0 +1,6 @@
+package com.trogiare.common.enumrate;
+
+public enum NotificationTypeEnum {
+
+
+}
--- src/main/java/com/trogiare/model/Notification.java
+++ src/main/java/com/trogiare/model/Notification.java
@@ -1,4 +1,28 @@
 package com.trogiare.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+@Entity
+@Table(name="notification")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Notification {
+        @Id
+        @GeneratedValue(generator = "objectid-generator")
+        @GenericGenerator(name = "objectid-generator", strategy = "com.trogiare.common.ObjectIDGenerator")
+        @Column(unique = true, nullable = false, length = 24)
+        private String id;
+        private String receiver;
+        private String message;
+        private String typeNotification;
+        private LocalDateTime timestamp;
+        private boolean isRead;
+
+
 }
